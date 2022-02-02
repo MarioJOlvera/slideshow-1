58688cf482ee93f1105a7955f9a4f9f9b549be5a
Mario Juárez Olvera <m.juarez.93@live.com>
2022-02-01 20:20:15 -0600

    Add files via upload

---
+++ src/Components/SlideComponent/dataSlider.js
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const dataSlider = [
+    {
+        id: uuidv4(),
+        title: 'Lorem ipsum',
+        subTitle: 'Lorem'
+    },
+    {
+        id: uuidv4(),
+        title: 'Lorem ipsum 2',
+        subTitle: 'Lorem 2'
+    },
+    {
+        id: uuidv4(),
+        title: 'Lorem ipsum 3',
+        subTitle: 'Lorem 3'
+    },
+];
+
+export default dataSlider;
